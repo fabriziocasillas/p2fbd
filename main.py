67e14156c418b94c557ecf3c7471bbf8dc8e4a96
fabrizio_pimentel <fabriziopimentel@ciencias.unam.mx>
2026-03-01 12:52:20 -0600

    kinda how i want the menu to work, idk

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from cliente import Cliente
+
 def main():
     opt = 4
 
@@ -15,9 +17,29 @@ def main():
             print("Insumos")
         elif opt == 3:
             print("Clientes")
+            menuc()
 
         
-    
+
+def menuc():
+    opc = 5
+    while(opc != 0):
+        try:
+            opc = int(input("Bienvenido al sistema manejador de clientes, presiona "
+                            "\n 1 para agregar clientes \n 2 para consultar clientes por ID \n 3 para editar un cliente \n 4 Para eliminar un cliente" \
+                            "\n 0 para salir  \n"))
+        except ValueError:
+            print("eso no es un numero")
+
+        if opc == 0:
+            print("Saliendo...")
+        elif opc == 1:
+            print("Sucursal")
+        elif opc == 2:
+            print("Insumos")
+        elif opc == 3:
+            print("Clientes")
+
 
 
 if __name__ == "__main__":
