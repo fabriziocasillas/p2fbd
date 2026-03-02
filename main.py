d60c76893d6f7dbf470608bfac99097bbe99f69d
norberto_lopez <norberto.13@ciencias.unam.mx>
2026-03-02 17:12:25 -0600

    Se cambia formato de la cli y nombre de variables en main.py

--- main.py
+++ main.py
@@ -3,167 +3,199 @@ from clienteedit import ClienteEdit
 from sucursal import Sucursal
 from sucursaledit import SucursalEdit
 
-def main():
-    opt = 4
 
-    while(opt != 0):
-        print("Bienvenido al sistema manejador de la famracia de otro mundo, presiona \n 1 para sucursal \n 2 para insumos \n 3 para clientes \n 0 para salir \n")
-        opt = recnum()
-        if opt == 0:
+def main():
+    option = 4
+
+    while option != 0:
+        print(
+            "¡¡¡Bienvenido al sistema manejador de la Farmacia de Otro Mundo!!! \n 1. Sucursal \n 2. Insumos \n 3. Clientes \n 0. Salir \n Elige una opción:"
+        )
+        option = verifica_entrada()
+        if option == 0:
             print("Saliendo...")
-        elif opt == 1:
-            print("Sucursales")
+        elif option == 1:
+            print("\nSUCURSALES:")
             menus()
-        elif opt == 2:
-            print("Insumos")
-        elif opt == 3:
-            print("Clientes")
+        elif option == 2:
+            print("\nINSUMOS:")
+        elif option == 3:
+            print("\nCLIENTES:")
             menuc()
 
-        
 
 def menus():
-    ops = 5
+    option = 5
     sed = SucursalEdit()
-    while(ops != 0):
-        print("Bienvenido al sistema manejador de sucursales, presiona "
-              "\n 1 para agregar sucursal \n 2 para consultar sucursal por ID \n 3 para editar una sucursal \n 4 para eliminar una sucursal"
-              "\n 0 para salir  \n")
-        ops = recnum()
+    while option != 0:
+        print(
+            "SISTEMA MANEJADOR DE SUCURSALES"
+            "\n 1. Agregar sucursal \n 2. Consultar sucursal por ID \n 3. Editar una sucursal \n 4. Eliminar una sucursal \n 0. Salir  \n Elige una opción:"
+        )
+        option = verifica_entrada()
 
-        if ops == 1:
-            print("dame el id de la sucursal \n")
+        if option == 1:
+            print("\nIngresa el id de la sucursal: ")
             ids = -1
-            while(ids == -1):
-                ids = recnum()
+            while ids == -1:
+                ids = verifica_entrada()
 
-            nms = input("dame el nombre de la sucursal \n ")
-            drs = input("dame la direccion \n ")
-            tls = input("dame el telefono \n ")
-            hrs = input("dame el horario (ej. 08:00-22:00) \n ")
-            grs = input("dame el nombre del gerente \n ")
-            nsuc = Sucursal(ids, nms, drs, tls, hrs, grs)
-            sed.agregar_sucursal(nsuc)
+            nombre_sucursal = input("Nombre de la sucursal: \n ")
+            direccion = input("Direccion: \n ")
+            telefono_sucursal = input("Telefono: \n ")
+            horario_sucursal = input("Horario de atención: (ej. 08:00-22:00) \n ")
+            gerente_sucursal = input("Nombre del gerente: \n ")
+            nueva_sucursal = Sucursal(
+                ids,
+                nombre_sucursal,
+                direccion,
+                telefono_sucursal,
+                horario_sucursal,
+                gerente_sucursal,
+            )
+            sed.agregar_sucursal(nueva_sucursal)
             print("sucursal agregada:")
-            print(nsuc)
+            print(nueva_sucursal)
 
-        elif ops == 2:
-            print("dame el id de la sucursal a buscar\n")
+        elif option == 2:
+            print("\nIngresa el id de la sucursal a buscar:")
             ids = -1
-            while(ids == -1):
-                ids = recnum()
-            suc = sed.buscar_por_id(ids)
-            if suc == []:
-                print("no se encontro la sucursal buscada")
+            while ids == -1:
+                ids = verifica_entrada()
+            sucursal = sed.buscar_por_id(ids)
+            if sucursal == []:
+                print("INFO: No se encontro la sucursal buscada. \n")
             else:
-                print(suc)
+                print(sucursal)
 
-        elif ops == 3:
-            print("sucursal a editar\n")
-            print("dame el id de la sucursal \n")
+        elif option == 3:
+            print("Sucursal a editar\n")
+            print("Ingresa el id de la sucursal: ")
             idsb = -1
-            while(idsb == -1):
-                idsb = recnum()
-            nms = input("dame el nombre de la sucursal \n ")
-            drs = input("dame la direccion \n ")
-            tls = input("dame el telefono \n ")
-            hrs = input("dame el horario (ej. 08:00-22:00) \n ")
-            grs = input("dame el nombre del gerente \n ")
-            nsuc = Sucursal(idsb, nms, drs, tls, hrs, grs)
+            while idsb == -1:
+                idsb = verifica_entrada()
+            nombre_sucursal = input("Nombre de la sucursal: \n ")
+            direccion = input("Direccion: \n ")
+            telefono_sucursal = input("Telefono: \n ")
+            horario_sucursal = input("Horario de atención: (ej. 08:00-22:00) \n ")
+            gerente_sucursal = input("Nombre del gerente: \n ")
+            sucursal_editada = Sucursal(
+                idsb,
+                nombre_sucursal,
+                direccion,
+                telefono_sucursal,
+                horario_sucursal,
+                gerente_sucursal,
+            )
             try:
-                sed.editar_sucursal(nsuc)
-                print("sucursal editada")
-                print(nsuc)
+                sed.editar_sucursal(sucursal_editada)
+                print("INFO: ¡Sucursal editada exitosamente!")
+                print(sucursal_editada + "\n")
             except ValueError as e:
                 print(f"error: {e}")
 
-        elif ops == 4:
-            print("dame el id de la sucursal a eliminar \n")
+        elif option == 4:
+            print("Ingresa el id de la sucursal a eliminar: \n")
             idse = -1
-            while(idse == -1):
-                idse = recnum()
+            while idse == -1:
+                idse = verifica_entrada()
             sucl = sed.buscar_por_id(idse)
             if sucl == []:
-                print("no se encontro la sucursal buscada")
+                print("INFO: No se encontro la sucursal buscada.")
             else:
                 sed.eliminar_sucursal(idse)
-                print("sucursal eliminada")
+                print("INFO: Sucursal eliminada correctamente.")
 
 
 def menuc():
-    opc = 5
+    option = 5
     ced = ClienteEdit()
-    while(opc != 0):
-        print("Bienvenido al sistema manejador de clientes, presiona "
-                            "\n 1 para agregar clientes \n 2 para consultar clientes por ID \n 3 para editar un cliente \n 4 Para eliminar un cliente" \
-                            "\n 0 para salir  \n")
-        opc = recnum()
+    while option != 0:
+        print(
+            "SISTEMA MANEJADOR DE CLIENTES \n 1. Agregar cliente \n 2. Consultar cliente por ID \n 3. Editar cliente \n 4. Eliminar cliente \n 0. Salir \n Elige una opicón:"
+        )
+        option = verifica_entrada()
 
-        if opc == 1:
+        if option == 1:
 
-            print("dame la id del cliente \n")
-            idc = -1
-            while(idc==-1):
-                idc = recnum()
-            
-            nmc = input("dame su nombre completo \n ")
-            cpc = input("dame su curp \n ")
-            tlc = input("dame  su telefono \n ")
-            cec = input("dame su correo electrónico \n ")
-            drc = input("dame dirección del cliente \n")
-            ncli = Cliente(idc, nmc, cpc, tlc, cec, drc)
-            ced.agregar_cliente(ncli)
-            print("cliente agregado:")
-            print(ncli)
+            print("\nIngresa la id del cliente: ")
+            id_cliente = -1
+            while id_cliente == -1:
+                id_cliente = verifica_entrada()
 
-        elif opc == 2:
+            nombre_completo = input("Ingresa nombre completo: \n ")
+            curp = input("Ingresa CURP: \n")
+            telefono_cliente = input("Ingresa su telefono: \n ")
+            correo_cliente = input("Ingresa su e-mail: \n ")
+            direccion_cliente = input("Ingresa dirección del cliente:\n")
+            nuevo_cliente = Cliente(
+                id_cliente,
+                nombre_completo,
+                curp,
+                telefono_cliente,
+                correo_cliente,
+                direccion_cliente,
+            )
+            ced.agregar_cliente(nuevo_cliente)
+            print("INFO: Cliente agregado correctamente.")
+            print(nuevo_cliente)
+
+        elif option == 2:
             print("dame la id del cliente a buscar\n")
-            idc = -1
-            while(idc==-1):
-                idc = recnum()
-            clie = ced.buscar_por_id(idc)
-            if clie == []:
-                print("no se encontro el cliente buscado")
+            id_cliente = -1
+            while id_cliente == -1:
+                id_cliente = verifica_entrada()
+            cliente = ced.buscar_por_id(id_cliente)
+            if cliente == []:
+                print("\nINFO: No se encontro el cliente con id: " + id_cliente)
             else:
-                print(clie)
+                print(cliente)
 
-        elif opc == 3:
-            print("cliente a editar\n")
-            print("dame la id del cliente \n")
-            idcb = -1
-            while(idcb==-1):
-                idcb = recnum()
-            nmc = input("dame su nombre completo \n ")
-            cpc = input("dame su curp \n ")
-            tlc = input("dame  su telefono \n ")
-            cec = input("dame su correo electrónico \n ")
-            drc = input("dame dirección del cliente \n")
-            ncli = Cliente(idcb, nmc, cpc, tlc, cec, drc)
-            ced.editar_cliente(ncli)
-            print("cliente editado")
-            print(ncli)
-        elif opc == 4:
-            print("dame la id del cliente \n")
-            idce = -1
-            while(idce==-1):
-                idce = recnum()
-            cliel = ced.buscar_por_id(idce)
-            if cliel == []:
-                print("no se encontro el cliente buscado")
+        elif option == 3:
+            print("Cliente a editar.")
+            print("\nIngresa la id del cliente: ")
+            id_cliente_editar = -1
+            while id_cliente_editar == -1:
+                id_cliente_editar = verifica_entrada()
+            nombre_completo = input("Ingresa nombre completo: \n ")
+            curp = input("Ingresa CURP: \n ")
+            telefono_cliente = input("Ingresa su telefono: \n ")
+            correo_cliente = input("Ingresa su e-mail: \n ")
+            direccion_cliente = input("Ingresa dirección del cliente:\n")
+            cliente_editado = Cliente(
+                id_cliente_editar,
+                nombre_completo,
+                curp,
+                telefono_cliente,
+                correo_cliente,
+                direccion_cliente,
+            )
+            ced.editar_cliente(cliente_editado)
+            print("\nINFO: Cliente editado correctamente. \n")
+            print(cliente_editado)
+        elif option == 4:
+            print("\nIngresa la id del cliente: ")
+            id_cliente_eliminar = -1
+            while id_cliente_eliminar == -1:
+                id_cliente_eliminar = verifica_entrada()
+            cliente_a_eliminar = ced.buscar_por_id(id_cliente_eliminar)
+            if cliente_a_eliminar == []:
+                print(
+                    "\nINFO: No se encontro el cliente con id: " + id_cliente_eliminar
+                )
             else:
-                ced.eliminar_cliente(idce)
-                print("cliente eliminado")
+                ced.eliminar_cliente(id_cliente_eliminar)
+                print("\nINFO: Cliente Eliminado")
 
-def recnum():
+
+def verifica_entrada():
     try:
-        opc = int(input(""))
+        option = int(input(""))
     except ValueError:
-        print("eso no es un numero")
-        opc = -1
+        print("¡Ingresa una entrada válida!\n")
+        option = -1
 
-    return opc
-
-
+    return option
 
 
 if __name__ == "__main__":
